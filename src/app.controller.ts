b33cf4b6a793de33100c023a3159a591c80cc654
Arthur Darwanto <75304260+arthur-da-1205@users.noreply.github.com>
2023-02-01 14:09:08 +0700

    jwt guard

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,15 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AppService } from './app.service';
+import { GetCurrentUserById, JwtAuthGuard } from './utils';
 
 @Controller('/')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @UseGuards(AuthGuard('jwt'))
+  @UseGuards(JwtAuthGuard)
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(@GetCurrentUserById() userId: number): string {
+    return this.appService.getHello(userId);
   }
 }
---
+++ src/utils/index.ts
@@ -0,0 +1,2 @@
+export * from './get-user-by-id.decorator';
+export * from './guards/jwt-guard.guard';
